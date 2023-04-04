15d0bc5ef2baea3de785f29afd217e72df687342
rohanakode490 <rohanakode52@gmail.com>
2023-04-04 20:31:34 +0530

    Deploy commit

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,11 +1,6 @@
 import Logo from "./AI-Planet-Logo.png";
 import Insight from "./Hand-holding-bulb-3D.png";
-import card1 from "./InterviewMe.png";
-import card2 from "./Lorem-Ipsum.png";
-import card3 from "./Potter-ipsum.png";
-import card4 from "./Pizza-Ipsum.png";
-import card5 from "./Figma-ipsum.png";
-import card6 from "./Office-ipsum.png";
+
 import Img from "./add-photo-alternate.png";
 
-export { Logo, Insight, card1, card2, card3, card4, card5, card6, Img };
+export { Logo, Insight, Img };
